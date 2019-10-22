73f3bcc8ba124a1b1c14f5dd67d224e854cac9cd
Adston Oliveira <suportetecnico.adston@gmail.com>
2019-10-21 22:57:14 -0200

    Inserindo lindo

--- src/ClientSide/Model/Thread/ThMinningBlock.java
+++ src/ClientSide/Model/Thread/ThMinningBlock.java
@@ -26,11 +26,9 @@ public class ThMinningBlock implements Runnable{
                 while ( !block.getHash().substring(0, block.getDifficulty()).equals(target) ) { // Divide o hash da posicao 0, ate a qta 0
                     block.setNonce(nonce++);
                     block.setHash( block.calculateHash() ); // o nonce serve para a quantidade de hash gerados...
-//                    block.setTimeStamp(System.currentTimeMillis());
                 } // Gera varios hash's, ate que algum contenha a qtde desejadas de 0 no inicio
 
                 pb.stopThis();
-//                t.interrupt();
             finish = true;
         }
         
